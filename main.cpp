924a94578f4ffa4900dc4e4c9125c078a18ce43c
Elviss Strazdins <elviss@elviss.lv>
2018-08-13 17:28:58 +0300

    Add device parameter

--- main.cpp
+++ main.cpp
@@ -12,37 +12,26 @@
 #include <unistd.h>
 
 // Silicon Labs CEL EM3588 USB Stick
-static const uint16_t CEL_VID = 0x10C4;
-static const uint16_t CEL_PID = 0x8A5E;
+//static const uint16_t CEL_VID = 0x10C4;
+//static const uint16_t CEL_PID = 0x8A5E;
 static const speed_t CEL_BAUD = B57600;
 static const bool CEL_XON_XOFF = true;
 static const bool CEL_RTS_CTS = false;
 
 // Silicon Labs WSTK (J-Link Pro OB)
-static const uint16_t WSTK_VID = 0x1366;
-static const uint16_t WSTK_PID = 0x0105;
+//static const uint16_t WSTK_VID = 0x1366;
+//static const uint16_t WSTK_PID = 0x0105;
 static const speed_t WSTK_BAUD = B115200;
 static const bool WSTK_XON_XOFF = false;
 static const bool WSTK_RTS_CTS = true;
 
-/*
-static const uint16_t VID = CEL_VID;
-static const uint16_t PID = CEL_PID;
-static const speed_t BAUD = CEL_BAUD;
-static const bool XON_XOFF = CEL_XON_XOFF;
-static const bool RTS_CTS = CEL_RTS_CTS;
-/*/
-static const uint16_t VID = WSTK_VID;
-static const uint16_t PID = WSTK_PID;
-static const speed_t BAUD = WSTK_BAUD;
-static const bool XON_XOFF = WSTK_XON_XOFF;
-static const bool RTS_CTS = WSTK_RTS_CTS;
-//*/
-
 class Device final
 {
 public:
-    explicit Device(const std::string& path)
+    explicit Device(const std::string& path,
+                    speed_t baud,
+                    bool xOnOff,
+                    bool rtsCts)
     {
         fd = open(path.c_str(), O_RDWR | O_NOCTTY);
 
@@ -51,8 +40,6 @@ public:
         struct termios settings;
         if (tcgetattr(fd, &settings) != 0)
             throw std::runtime_error("Failed to get attributes");
-
-        const speed_t baud = BAUD; // baud rate
 
         if (cfsetospeed(&settings, baud) != 0) // baud rate
             throw std::runtime_error("Failed to set baud rate");
@@ -67,9 +54,9 @@ public:
         //settings.c_oflag &= ~OPOST; // raw output
 
         // hardware flow control
-        if (RTS_CTS) settings.c_cflag |= CRTSCTS;
+        if (rtsCts) settings.c_cflag |= CRTSCTS;
         // software flow control
-        if (XON_XOFF) settings.c_iflag |= IXON | IXOFF | IXANY;
+        if (xOnOff) settings.c_iflag |= IXON | IXOFF | IXANY;
 
         if (tcsetattr(fd, TCSANOW, &settings) != 0) // apply the settings
             throw std::runtime_error("Failed to set attributes");
@@ -95,7 +82,7 @@ public:
             printf(" %x", c);
         printf("\n");
 
-        int remaining = data.size();
+        int remaining = static_cast<int>(data.size());
         const uint8_t* ptr = static_cast<const uint8_t*>(data.data());
 
         while (remaining)
@@ -145,6 +132,9 @@ int main(int argc, const char * argv[])
     try
     {
         std::string path;
+        speed_t baud = CEL_BAUD;
+        bool xOnOff = CEL_XON_XOFF;
+        bool rtsCts = CEL_RTS_CTS;
 
         for (int i = 1; i < argc; ++i)
         {
@@ -153,11 +143,31 @@ int main(int argc, const char * argv[])
                 if (++i >= argc) throw std::runtime_error("Invalid parameter");
                 path = argv[i];
             }
+            else if (std::string(argv[i]) == "--device")
+            {
+                if (++i >= argc) throw std::runtime_error("Invalid parameter");
+
+                if (std::string(argv[i]) == "CEL")
+                {
+                    baud = CEL_BAUD;
+                    xOnOff = CEL_XON_XOFF;
+                    rtsCts = CEL_RTS_CTS;
+                }
+                else if (std::string(argv[i]) == "WSTK")
+                {
+                    baud = WSTK_BAUD;
+                    xOnOff = WSTK_XON_XOFF;
+                    rtsCts = WSTK_RTS_CTS;
+                }
+                throw std::runtime_error("Invalid device");
+            }
+            else
+                throw std::runtime_error("Invalid parameter");
         }
 
         if (path.empty()) throw std::runtime_error("No path");
 
-        Device device(path);
+        Device device(path, baud, xOnOff, rtsCts);
 
         while (!device.isClosed())
             device.readData();
