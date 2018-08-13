4889771311fe6b3b50021598f4f05d65a90fc72a
Elviss Strazdins <elviss@elviss.lv>
2018-08-13 19:11:51 +0300

    Implement Windows part

--- main.cpp
+++ main.cpp
@@ -37,9 +37,68 @@ public:
                     bool xOnOff,
                     bool rtsCts)
     {
+#ifdef _WIN32
+        int size = MultiByteToWideChar(CP_UTF8, 0, path.c_str(), -1, nullptr, 0);
+        if (size == 0)
+            throw std::runtime_error("Failed to convert UTF-8 to wide char");
+
+        std::vector<WCHAR> buffer(size);
+        if (MultiByteToWideChar(CP_UTF8, 0, path.c_str(), -1, buffer.data(), size) == 0)
+            throw std::runtime_error("Failed to convert the filename to wide char");
+
+        // relative paths longer than MAX_PATH are not supported
+        if (buffer.size() > MAX_PATH)
+            buffer.insert(buffer.begin(), { L'\\', L'\\', L'?', L'\\' });
+
+        handle = CreateFile(buffer.data(), GENERIC_READ | GENERIC_WRITE, 0,
+                            nullptr, OPEN_EXISTING, 0, nullptr);
+
+        if (handle == INVALID_HANDLE_VALUE)
+            throw std::runtime_error("Failed to open " + path);
+
+        DCB serialParameters;
+        serialParameters.DCBlength = sizeof(serialParameters);
+
+        if (!GetCommState(handle, &serialParameters))
+            throw std::runtime_error("Failed to get parameters");
+
+        DWORD speed;
+
+        switch (baud)
+        {
+            case 110: speed = CBR_110; break;
+            case 300: speed = CBR_300; break;
+            case 600: speed = CBR_600; break;
+            case 1200: speed = CBR_1200; break;
+            case 2400: speed = CBR_2400; break;
+            case 4800: speed = CBR_4800; break;
+            case 9600: speed = CBR_9600; break;
+            case 19200: speed = CBR_19200; break;
+            case 38400: speed = CBR_38400; break;
+            case 57600: speed = CBR_57600; break;
+            case 115200: speed = CBR_115200; break;
+            case 128000: speed = CBR_128000; break;
+            case 256000: speed = CBR_256000; break;
+            default:
+                throw std::runtime_error("Invalid baud rate");
+        }
+
+        serialParameters.BaudRate = speed;
+        serialParameters.ByteSize = 8;
+        serialParameters.StopBits = ONESTOPBIT;
+        serialParameters.fParity = NOPARITY;
+        serialParameters.fInX = xOnOff ? 1 : 0;
+        serialParameters.fOutX = xOnOff ? 1 : 0;
+        serialParameters.fRtsControl = rtsCts ? 1 : 0;
+
+        if (!SetCommState(handle, &serialParameters))
+            throw std::runtime_error("Failed to set parameters");
+
+#else
         fd = open(path.c_str(), O_RDWR | O_NOCTTY);
 
-        if (fd == -1) throw std::runtime_error("Failed to open " + path);
+        if (fd == -1)
+            throw std::runtime_error("Failed to open " + path);
 
         struct termios settings;
         if (tcgetattr(fd, &settings) != 0)
@@ -94,11 +153,16 @@ public:
 
         usleep(200000);
         tcflush(fd,TCIOFLUSH);
+#endif
     }
 
     ~Device()
     {
+#ifdef _WIN32
+        if (handle == INVALID_HANDLE_VALUE) CloseHandle(handle);
+#else
         if (fd != -1) close(fd);
+#endif
     }
 
     Device(const Device&) = delete;
@@ -113,6 +177,17 @@ public:
             printf(" %x", c);
         printf("\n");
 
+#ifdef _WIN32
+        DWORD remaining = static_cast<DWORD>(data.size());
+        const uint8_t* ptr = static_cast<const uint8_t*>(data.data());
+
+        DWORD bytesWritten;
+        if (!WriteFile(handle, ptr, remaining, &bytesWritten, nullptr))
+            throw std::runtime_error("Failed to read data");
+
+        ptr += bytesWritten;
+        remaining -= bytesWritten;
+#else
         int remaining = static_cast<int>(data.size());
         const uint8_t* ptr = static_cast<const uint8_t*>(data.data());
 
@@ -125,6 +200,7 @@ public:
             ptr += result;
             remaining -= result;
         }
+#endif
     }
 
     std::vector<uint8_t> readData()
@@ -132,6 +208,13 @@ public:
         uint8_t buffer[256];
         std::vector<uint8_t> data;
 
+#ifdef _WIN32
+        DWORD bytesRead;
+        if (!ReadFile(handle, buffer, sizeof(buffer), &bytesRead, nullptr))
+            throw std::runtime_error("Failed to read data");
+
+        data.assign(buffer, buffer + bytesRead);
+#else
         ssize_t result = read(fd, buffer, sizeof(buffer));
 
         if (result == -1) throw std::runtime_error("Failed to read data");
@@ -142,7 +225,8 @@ public:
             throw std::runtime_error("Connection closed");
         }
 
-        data.assign(std::begin(buffer), std::end(buffer));
+        data.assign(buffer, buffer + result);
+#endif
 
         printf("Received:");
         for (uint8_t c : data)
